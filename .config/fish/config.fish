09ef03345987f152582848bee73930fb0d73654c
Mikhail Mikhailenko <mikhail.mikhailenko@icloud.com>
2021-01-27 01:45:16 +0100

    Change fish colors to light; Add Conda; Drop asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,7 @@
-set --erase fish_greeting
-source /usr/local/opt/asdf/asdf.fish
+set fish_greeting
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/s303/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
