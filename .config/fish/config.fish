7c92cc3bd53039a0ce1c37a2ca90bb73ed7a0be2
Mikhail Mikhailenko <mikhail.mikhailenko@icloud.com>
2022-07-08 15:28:17 +0300

    - Dropped obsoleete "fish over bash" idea as now both direnv and conda support fish out-of-the box - Remove fish / iTerm integration - Added out-of-the box direnv and conda support fish support - Speed up conda init - Conda fish prompt improvement - Don't activate base conda environment by default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,1 @@
 set fish_greeting
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval $HOME/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
-source ~/.iterm2_shell_integration.fish
--- .config/fish/functions/fish_s303_conda_prompt.fish
+++ .config/fish/functions/fish_s303_conda_prompt.fish
@@ -1,5 +1,5 @@
 function fish_s303_conda_prompt
-if test "$CONDA_DEFAULT_ENV" != "base"
+if set -q CONDA_DEFAULT_ENV; and test "$CONDA_DEFAULT_ENV" != "base"
     echo -s (set_color magenta) "($CONDA_DEFAULT_ENV) "
 end
 end
