850fb520c9b746f3ed6205ed7c7fbe9f125fc634
Mik Mikhailenko <sniff303@yandex.ru>
2025-01-25 14:29:15 +0400

    Save latest updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,8 @@
 set fish_greeting
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set -gx MAMBA_EXE /opt/homebrew/opt/micromamba/bin/micromamba
+set -gx MAMBA_ROOT_PREFIX /Users/s303/micromamba
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
